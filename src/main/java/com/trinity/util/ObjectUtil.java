efb7748e04304ae8b007a8d481670ff7cc4dea9a
user1 <user1@1076962010998015.onaliyun.com>
2023-02-20 17:12:43 +0800

    version 0.4.0

--- src/main/java/com/trinity/util/ObjectUtil.java
+++
@@ -1,9 +0,0 @@
-package com.trinity.util;
-
-public class ObjectUtil {
-
-    public static Integer ObjectToInteger(Object o) {
-        return Integer.valueOf(String.valueOf(o));
-    }
-
-}
--- src/main/java/com/trinity/util/SaltRandom.java
+++
@@ -1,9 +0,0 @@
-package com.trinity.util;
-
-import java.util.UUID;
-
-public class SaltRandom {
-    public String getRandomSalt(){
-        return UUID.randomUUID().toString().toUpperCase();
-    }
-}
