7beee6a88793a418798928708fbc49251150186d
user1 <user1@1076962010998015.onaliyun.com>
2023-03-18 12:10:40 +0800

    version 1.0.4 release

--- shop/src/main/java/com/shop/config/WebPathConfig.java
+++ shop/src/main/java/com/shop/config/WebPathConfig.java
@@ -1,7 +1,7 @@
-package com.video.config;
+package com.shop.config;
 
-import com.video.util.XMLUtil;
-import com.video.util.XMLUtil2;
+
+import com.shop.util.XMLUtil2;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,8 +11,6 @@ public class WebPathConfig implements WebMvcConfigurer {
     //将静态文件映射到“/”
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/video/**")
-                .addResourceLocations("file:" + XMLUtil.getText());
         registry.addResourceHandler("/picture/**")
                 .addResourceLocations("file:" + XMLUtil2.getText());
     }
